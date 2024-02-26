2c8485becb59fd1eb1e0eb3f9b4021ad0b839e7d
Tristan <tristandevelop@protonmail.com>
2024-02-26 16:35:18 +0100

    Make popcorn! 🍿 Co-authored-by: AmberCDH kanarieamber@gmail.com

---
+++ PopcornMachine/CaramelPopcorn.cs
@@ -0,0 +1,16 @@
+﻿
+namespace sofa3.PopcornMachine
+{
+    public class CaramelPopcorn : PopcornRecipe
+    {
+        public override void AddTopping()
+        {
+            Console.WriteLine("Adds a chocolate topping to the popcorn");
+        }
+
+        public override void ChooseTaste()
+        {
+            Console.WriteLine("Adds caramel");
+        }
+    }
+}
---
+++ PopcornMachine/SweetPopcorn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sofa3.PopcornMachine
+{
+    public class SweetPopcorn : PopcornRecipe
+    {
+        public override void AddTopping()
+        {
+            Console.WriteLine("Adds a caramel topping to the popcorn");
+        }
+
+        public override void ChooseTaste()
+        {
+            Console.WriteLine("Adds sugarwater");
+        }
+    }
+}
